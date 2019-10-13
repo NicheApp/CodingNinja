54de6b98a543e7b046704ebe27d7ac3e1395a110
ankit <ankithans1947@gmail.com>
2019-10-13 17:42:22 +0530

    marks

---
+++ app/src/main/java/com/ankithans/attendence/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ankithans.attendence;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
